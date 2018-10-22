ad4fda6ff540cc6bd08ab55f1eb7b618cf0d6877
ChuckBTaylor <WLScripps@gmail.com>
2018-10-22 09:00:57 -0500

    Updating `.sql` file

--- sql/gametracker.sql
+++ sql/gametracker.sql
@@ -21,13 +21,36 @@ CREATE TABLE
     first_name VARCHAR,
     last_name VARCHAR,
     team_id BIGINT NULL,
+    PRIMARY KEY (ID),
     FOREIGN KEY (team_id) REFERENCES team(ID)
   );
- INSERT INTO player (ID, first_name, last_name) VALUES (1, "Squeak", "Moore");
+  INSERT INTO player (ID, first_name, last_name) VALUES (1, "Squeak", "Moore");
 
- CREATE TABLE
+CREATE TABLE
  hibernate_sequence_player (
    next_val BIGINT
  );
 
-INSERT INTO hibernate_sequence_player (next_val) VALUES (1);
+  INSERT INTO hibernate_sequence_player (next_val) VALUES (1);
+
+CREATE TABLE
+ game
+ (
+   ID BIGINT NOT NULL,
+   date_of_game VARCHAR,
+   winning_team_id BIGINT NULL,
+   location_of_game VARCHAR,
+   away_team_score INT,
+   home_team_score INT,
+   home_team_id BIGINT NULL,
+   away_team_id BIGINT NULL,
+   PRIMARY KEY (ID),
+   FOREIGN KEY (winning_team_id) REFERENCES team(ID),
+   FOREIGN KEY (away_team_id) REFERENCES team(ID),
+   FOREIGN KEY (home_team_id) REFERENCES team(ID)
+ );
+ 
+CREATE TABLE
+ hibernate_sequence_game (
+   next_val BIGINT
+ );
